9b05bbcbb778696b726de1c1a65957f4a9a169a5
bearnardoriain <bearnard.oriain@gmail.com>
2020-04-08 13:07:40 +0100

    refactored code to render only three Part components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,54 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
-  )
-}
+// similar to Header in exercise 1.1
+const Header = ({course}) => (
+  <h1>
+    {course}
+  </h1>
+)
+// similar to Content in exercise 1.1
+const Part = ({part_name, number_of_exercises}) => (
+  <p>
+    {part_name} {number_of_exercises}
+  </p>
+)
 
-const Content = (props) => {
-  return (
-    <div>
-      <p>
-        {props.parts} {props.exercises}
-      </p>
-    </div>
+/* each (part*) and (exercises*) is a Part,
+  therefore the Parts can be stored as an array in
+  the App component
+  in JavaScript, the array can be looped through
+  using the map() function */
+const Content = ({parts}) => (
+  parts.map(part => (
+      // name of each Part used as identifier
+      <Part key = {part.part_name}
+      part_name = {part.part_name}
+      number_of_exercises = {part.number_of_exercises} />
+    )
   )
-}
+)
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises {props.total_exercises}
-      </p>
-    </div>
-  )
-}
+/* addition method, used for adding
+  number_of_exercises in Total */
+const add = (a, b) => a + b
+
+const Total = ({parts}) => (
+  <p>
+    Number of exercises {parts.map(part => part.number_of_exercises).reduce(add)}
+  </p>
+)
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half-stack application development',
+    parts: [
+      {part_name: 'Fundamentals of React', number_of_exercises: 10},
+      {part_name: 'Using props to pass data', number_of_exercises: 7},
+      {part_name: 'State of a component', number_of_exercises: 14}
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {part1} exercises = {exercises1}/>
-      <Content parts = {part2} exercises = {exercises2}/>
-      <Content parts = {part3} exercises = {exercises3}/>
-      <Total total_exercises = {exercises1 + exercises2 + exercises3}/>
+      <Header course = {course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, document.getElementById('root')
+)
