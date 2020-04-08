a8396a8ca77e1fea59729d98a5f6a88bd3e75ba8
bearnardoriain <bearnard.oriain@gmail.com>
2020-04-08 09:56:25 +0100

    refactored code to include three new components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.parts} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.total_exercises}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +42,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course}/>
+      <Content parts = {part1} exercises = {exercises1}/>
+      <Content parts = {part2} exercises = {exercises2}/>
+      <Content parts = {part3} exercises = {exercises3}/>
+      <Total total_exercises = {exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
