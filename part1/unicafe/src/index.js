3d48d1a9dbfd23de86f07f0eb52ae5afc525baaa
bearnardoriain <bearnard.oriain@gmail.com>
2020-05-25 22:10:26 +0100

    completed exercise 1.7, first attempt at exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+/* exercise 1.8 */
+const Statistics = (props) => {
+  return (
+    <p>{props.text}{props.counter}</p>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const totalScore = good + (neutral * 0) + (bad * -1)
+  const average = totalScore / total
+  const positive = (good / total) * 100
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>
+          good
+        </button>
+        <button onClick={() => setNeutral(neutral + 1)}>
+          neutral
+        </button>
+        <button onClick={() => setBad(bad + 1)}>
+          bad
+        </button>
+      </div>
+      {/* exercise 1.7 */}
+      <h1>Statistics</h1>
+      <div>
+        { /* exercise 1.7 */
+        /* <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>sum {total}</p>
+        <p>average {average}</p>
+        <p>positive {positive + '%'}</p> */}
+        {/* exercise 1.8 */}
+        <Statistics text = 'good' counter = {good}/>
+        <Statistics text = 'neutral' counter = {neutral}/>
+        <Statistics text = 'bad' counter = {bad}/>
+        <Statistics text = 'sum' counter = {total}/>
+        <Statistics text = 'average' counter = {average}/>
+        <Statistics text = 'positive' counter = {`${positive}%`}/>
+      </div>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
