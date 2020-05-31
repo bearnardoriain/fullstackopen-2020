50acf3879e6e1a0172d9358278d2655e783af0b7
bearnardoriain <bearnard.oriain@gmail.com>
2020-05-31 09:16:37 +0100

    first attempt at exercise 1.11 - table displaying statistics before feedback has been given

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -21,6 +21,7 @@ const Statistics = ({good, neutral, bad}) => {
       <Statistic text = "good" counter = {good}/>
       <Statistic text = "neutral" counter = {neutral}/>
       <Statistic text = "bad" counter = {bad}/>
+      <Statistic text = "sum" counter = {good + neutral + bad}/>
       <Statistic text = "average" counter = {(good + (neutral * 0) + (bad * -1))/(good + neutral + bad)}/>
       <Statistic text = "positive" counter = {`${(good / (good + neutral + bad)) * 100}%`}/>
     </div>
@@ -68,6 +69,7 @@ const App = () => {
         <button onClick={() => setBad(bad + 1)}>
           bad
         </button> */}
+        {/* exercise 1.10 */}
         <Button handleClick={() => incrementGood(good)} text = "good"/>
         <Button handleClick={() => incrementNeutral(neutral)} text = "neutral"/>
         <Button handleClick={() => incrementBad(bad)} text = "bad"/>
@@ -88,8 +90,32 @@ const App = () => {
         <Statistic text = 'sum' counter = {total}/>
         <Statistic text = 'average' counter = {average}/>
         <Statistic text = 'positive' counter = {`${positive}%`}/> */}
-        {/* exercise 1.9 */}
-        <Statistics good = {good} neutral = {neutral} bad = {bad} average = {average} positive = {positive}/>
+        {/* exercises 1.9 and 1.10b */}
+        {/* <Statistics good = {good} neutral = {neutral} bad = {bad} sum = {total} average = {average} positive = {positive}/> */}
+        {/* exercise 1.11 */}
+        <table>
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td><Statistics good = {good}/></td>
+            </tr>
+            <tr>
+              <td><Statistics neutral = {neutral}/></td>
+            </tr>
+            <tr>
+              <td><Statistics bad = {bad}/></td>
+            </tr>
+            <tr>
+              <td><Statistics sum = {total}/></td>
+            </tr>
+            <tr>
+              <td><Statistics average = {average}/></td>
+            </tr>
+            <tr>
+              <td><Statistics positive = {positive}/></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
