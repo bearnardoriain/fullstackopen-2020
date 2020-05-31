810eb64b8c25b8341f5c6a4b314d28b418ca0069
bearnardoriain <bearnard.oriain@gmail.com>
2020-05-31 13:35:27 +0100

    completed exercise 1.11, fixed table formatting

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,22 +1,46 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-/* exercises 1.8 and 1.10b */
-const Statistic = (props) => {
-  console.log(props)
-  return (
-    <p>{props.text} {props.counter}</p>
-  )
-}
+/* exercise 1.10a */
+const Button = ({handleClick, text}) => (
+  <button onClick = {handleClick}>
+    {text}
+  </button>
+)
 
-/* exercise 1.9 */
+/* exercises 1.8 and 1.10b */
+/* const Statistic = ({text, counter}) => {
+  return (
+    <p>{text}{counter}</p>
+  )
+} */
+
+/* exercise 1.11 */
+const Statistic = ({text, counter}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{counter}</td>
+  </tr>
+)
+
+
+/* exercises 1.9 and 1.11 */
 const Statistics = ({good, neutral, bad}) => {
-  if ((good === 0) && (neutral === 0) && (bad === 0)) {
+  const total = good + neutral + bad
+  /* exercise 1.9 and 1.10 */
+  /* if ((good === 0) && (neutral === 0) && (bad === 0)) {
+    return (
+      <p>No feedback given</p>
+    )
+  } */
+  /* exercise 1.11 */
+  if (total === 0) {
     return (
       <p>No feedback given</p>
     )
   }
-  return (
+  /* exercises 1.9 and 1.10 */
+  /* return (
     <div>
       <Statistic text = "good" counter = {good}/>
       <Statistic text = "neutral" counter = {neutral}/>
@@ -25,27 +49,39 @@ const Statistics = ({good, neutral, bad}) => {
       <Statistic text = "average" counter = {(good + (neutral * 0) + (bad * -1))/(good + neutral + bad)}/>
       <Statistic text = "positive" counter = {`${(good / (good + neutral + bad)) * 100}%`}/>
     </div>
-  )
+  ) */
+  /* exercise 1.11 */
+  else {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <Statistic text = "good" counter = {good}/>
+            <Statistic text = "neutral" counter = {neutral}/>
+            <Statistic text = "bad" counter = {bad}/>
+            <Statistic text = "sum" counter = {good + neutral + bad}/>
+            <Statistic text = "average" counter = {(good + (bad * -1)) / total}/>
+            <Statistic text = "positive" counter = {`${(good / total) * 100}%`}/>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
-
-/* exercise 1.10a */
-const Button = (props) => (
-  <button onClick = {props.handleClick}>
-    {props.text}
-  </button>
-)
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
+  /* exercises 1.7 to 1.10 */
+  /* const total = good + neutral + bad
   const totalScore = good + (neutral * 0) + (bad * -1)
   const average = totalScore / total
-  const positive = (good / total) * 100
+  const positive = (good / total) * 100 */
+
   /* exercise 1.10a */
-  const incrementGood = good => {
+  /* const incrementGood = good => {
     setGood(good + 1)
   }
   const incrementNeutral = neutral => {
@@ -53,7 +89,11 @@ const App = () => {
   }
   const incrementBad = bad => {
     setBad(bad + 1)
-  }
+  } */
+  /* exercise 1.11 */
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
 
   return (
     <div>
@@ -70,9 +110,13 @@ const App = () => {
           bad
         </button> */}
         {/* exercise 1.10 */}
-        <Button handleClick={() => incrementGood(good)} text = "good"/>
-        <Button handleClick={() => incrementNeutral(neutral)} text = "neutral"/>
-        <Button handleClick={() => incrementBad(bad)} text = "bad"/>
+        {/* <Button handleClick = {() => incrementGood(good)} text = "good"/>
+        <Button handleClick = {() => incrementNeutral(neutral)} text = "neutral"/>
+        <Button handleClick = {() => incrementBad(bad)} text = "bad"/> */}
+        {/* exercise 1.11 */}
+        <Button handleClick = {incrementGood} text = "good"/>
+        <Button handleClick = {incrementNeutral} text = "neutral"/>
+        <Button handleClick = {incrementBad} text = "bad"/>
       </div>
       <h1>Statistics</h1>
       <div>
@@ -90,32 +134,12 @@ const App = () => {
         <Statistic text = 'sum' counter = {total}/>
         <Statistic text = 'average' counter = {average}/>
         <Statistic text = 'positive' counter = {`${positive}%`}/> */}
+        {/* exercise 1.9 */}
+        {/* <Statistics good = {good} neutral = {neutral} bad = {bad} average = {average} positive = {positive}/> */}
         {/* exercises 1.9 and 1.10b */}
         {/* <Statistics good = {good} neutral = {neutral} bad = {bad} sum = {total} average = {average} positive = {positive}/> */}
         {/* exercise 1.11 */}
-        <table>
-          <thead></thead>
-          <tbody>
-            <tr>
-              <td><Statistics good = {good}/></td>
-            </tr>
-            <tr>
-              <td><Statistics neutral = {neutral}/></td>
-            </tr>
-            <tr>
-              <td><Statistics bad = {bad}/></td>
-            </tr>
-            <tr>
-              <td><Statistics sum = {total}/></td>
-            </tr>
-            <tr>
-              <td><Statistics average = {average}/></td>
-            </tr>
-            <tr>
-              <td><Statistics positive = {positive}/></td>
-            </tr>
-          </tbody>
-        </table>
+        <Statistics good = {good} neutral = {neutral} bad = {bad}/>
       </div>
     </div>
   )
